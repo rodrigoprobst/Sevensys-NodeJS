e5f28660cd4cc562c64e9f9db46c2fa148d5d344
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2019-08-15 21:17:13 -0300

    removido tudo que for desnecessario para poder adicionar no gitignore

--- Aula03/dist/src/services/test/config/mysql.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_typescript_1 = require("sequelize-typescript");
-const mysql_1 = require("../../../../config/mysql");
-mysql_1.mysqlDefaults.options.models = [__dirname + '/../models'];
-exports.sequelize = new sequelize_typescript_1.Sequelize(mysql_1.mysqlDefaults.database, mysql_1.mysqlDefaults.user, mysql_1.mysqlDefaults.password, 
-// @ts-ignore
-mysql_1.mysqlDefaults.options);
-//# sourceMappingURL=mysql.js.map
